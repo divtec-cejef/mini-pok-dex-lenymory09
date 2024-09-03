4375911912451d3bb5f6202e6bc34686f4ea909e
Leny Mory <leny.mory@divtec.ch>
2024-09-03 16:26:54 +0200

    etape 1 quasiment fini

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
 // Couleur par défaut pour les types de Pokémon non définis
 const DEFAULT_COLOR = '#ccc';
 
+const pokemonContainer = document.querySelector(".pokemon-container");
+
 // Couleurs pour chaque type de Pokémon
 const typeColors = {
     'Électrique': '#FFD700',
@@ -28,22 +30,37 @@ const typeColors = {
 };
 
 // Tableau d'objets représentant les Pokémon
-const pokemons = [
-    { name: 'Pikachu', type: 'Électrique', level: 35, img: 'pikachu.png' },
-    { name: 'Bulbizarre', type: 'Plante,Poison', level: 15, img: 'bulbizarre.png' },
-    { name: 'Salamèche', type: 'Feu', level: 20, img: 'salameche.png' },
-    { name: 'Carapuce', type: 'Eau', level: 10, img: 'carapuce.png' },
-    { name: 'Rondoudou', type: 'Normal,Fée', level: 25, img: 'rondoudou.png' },
-    { name: 'Ectoplasma', type: 'Spectre,Poison', level: 45, img: 'ectoplasma.png' },
-    { name: 'Évoli', type: 'Normal,Combat', level: 22, img: 'evoli.png' },
-    { name: 'Dracaufeu', type: 'Feu,Vol', level: 50, img: 'dracaufeu.png' },
-    { name: 'Florizarre', type: 'Plante,Poison', level: 55, img: 'florizarre.png' },
-    { name: 'Tortank', type: 'Eau', level: 52, img: 'tortank.png' },
-    { name: 'Mélofée', type: 'Fée', level: 18, img: 'melofee.png' },
-    { name: 'Raichu', type: 'Électrique', level: 40, img: 'raichu.png' },
-    { name: 'Magicarpe', type: 'Eau', level: 5, img: 'magicarpe.png' },
-    { name: 'Lokhlass', type: 'Eau,Glace', level: 35, img: 'lokhlass.png' },
-    { name: 'Onix', type: 'Roche,Sol', level: 30, img: 'onix.png' },
-    { name: 'Ronflex', type: 'Normal', level: 45, img: 'ronflex.png' },
-    { name: 'Mewtwo', type: 'Psy', level: 70, img: 'mewtwo.png' }
+const pokemonsTab = [
+    {name: 'Pikachu', type: 'Électrique', level: 35, img: 'pikachu.png'},
+    {name: 'Bulbizarre', type: 'Plante,Poison', level: 15, img: 'bulbizarre.png'},
+    {name: 'Salamèche', type: 'Feu', level: 20, img: 'salameche.png'},
+    {name: 'Carapuce', type: 'Eau', level: 10, img: 'carapuce.png'},
+    {name: 'Rondoudou', type: 'Normal,Fée', level: 25, img: 'rondoudou.png'},
+    {name: 'Ectoplasma', type: 'Spectre,Poison', level: 45, img: 'ectoplasma.png'},
+    {name: 'Évoli', type: 'Normal,Combat', level: 22, img: 'evoli.png'},
+    {name: 'Dracaufeu', type: 'Feu,Vol', level: 50, img: 'dracaufeu.png'},
+    {name: 'Florizarre', type: 'Plante,Poison', level: 55, img: 'florizarre.png'},
+    {name: 'Tortank', type: 'Eau', level: 52, img: 'tortank.png'},
+    {name: 'Mélofée', type: 'Fée', level: 18, img: 'melofee.png'},
+    {name: 'Raichu', type: 'Électrique', level: 40, img: 'raichu.png'},
+    {name: 'Magicarpe', type: 'Eau', level: 5, img: 'magicarpe.png'},
+    {name: 'Lokhlass', type: 'Eau,Glace', level: 35, img: 'lokhlass.png'},
+    {name: 'Onix', type: 'Roche,Sol', level: 30, img: 'onix.png'},
+    {name: 'Ronflex', type: 'Normal', level: 45, img: 'ronflex.png'},
+    {name: 'Mewtwo', type: 'Psy', level: 70, img: 'mewtwo.png'}
 ];
+
+function displayPokemons() {
+    pokemonContainer.textContent = "";
+
+    if (!pokemonsTab.length) {
+        pokemonContainer.innerHTML = '<p>Dracaufeu a tout brûlé, aucun Pokémon ne correspond à ta recherche !</p>'
+        return;
+    }
+
+    for (let pokemon of pokemonsTab) {
+        pokemonContainer.innerHTML += `<p>${pokemon.name}</p>`;
+    }
+}
+
+displayPokemons();
